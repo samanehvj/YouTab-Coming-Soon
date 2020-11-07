688a696023a7cfb6d398de7f25338711f588bad7
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-07 12:55:11 -0800

    Add index.php into view

---
+++ app/views/index.php
@@ -0,0 +1,8 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+<h1>Welcome to YouTab Home Page</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iure tenetur facere, sed inventore voluptates distinctio, atque, quisquam alias deleniti porro quidem praesentium eum dolor ipsum nam? Explicabo, aliquam omnis.
+</p>
+
+<?php include VIEW . 'layouts/footer.php' ?>
