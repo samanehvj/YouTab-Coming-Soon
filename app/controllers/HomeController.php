2b573575767fcb6150c262a2816b6fe6f29073eb
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-07 12:47:50 -0800

    Create HomeController.php

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+        $this->setView('index');
+        $this->view->pageTitle = SITENAME . " - HOME";
+        $this->view->render();
+    }
+
+    public function about()
+    {
+        $this->setView('about');
+        $this->view->pageTitle = SITENAME . " - ABOUT";
+        $this->view->render();
+    }
+
+    public function contact()
+    {
+        $this->setView('contact');
+        $this->view->pageTitle = SITENAME . " - Contact";
+        $this->view->render();
+    }
+
+    public function comingSoon()
+    {
+    }
+
+    public function pageNotFound()
+    {
+        $this->setView('pageNotFound');
+        $this->view->render();
+    }
+}
