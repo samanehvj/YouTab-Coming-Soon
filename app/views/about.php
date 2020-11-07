d1158dc9f30cd7965a54d7ae595da4483f2cee3e
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-07 12:56:05 -0800

    Add about.php into view

---
+++ app/views/about.php
@@ -0,0 +1,8 @@
+<?php include VIEW . 'layouts/header.php' ?>
+
+<h1>YouTab Bio</h1>
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis ut eius id repellat reprehenderit aperiam tempore quae doloribus fugiat asperiores nesciunt dignissimos nam, soluta voluptates fuga corporis enim. Dignissimos, ab?
+</p>
+
+<?php include VIEW . 'layouts/footer.php' ?>
