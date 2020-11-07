0fbabd881c5a13e9541621f402cb36d6a37eedb5
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-07 11:12:11 -0800

    Define path and routes in index.php and autoload theme

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+define('SITENAME', 'YouTab Brand');
+define('URLROOT', 'http://localhost:8585');
+
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
+define('VIEW', APP . 'views' . DIRECTORY_SEPARATOR);
+define('MODEL', APP . 'models' . DIRECTORY_SEPARATOR);
+define('CONTROLLER', APP . 'controllers' . DIRECTORY_SEPARATOR);
+define('LIB', APP . 'libs' . DIRECTORY_SEPARATOR);
+
+$modules = [ROOT, APP, VIEW, MODEL, CONTROLLER, LIB];
+set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules));
+spl_autoload_register('spl_autoload', false);
