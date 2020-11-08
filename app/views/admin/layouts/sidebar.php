8ac34324d08838aa7be863cb04a044b36bc09fb8
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 13:31:38 -0800

    Add sidebar for dashbard area

---
+++ app/views/admin/layouts/sidebar.php
@@ -0,0 +1,48 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="sidebar-heading">YouTab </div>
+        <div class="list-group list-group-flush">
+            <a href="dashboard.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+            <a href="pages.php" class="list-group-item list-group-item-action bg-light">Pages</a>
+            <a href="navigation.php" class="list-group-item list-group-item-action bg-light">Navigation</a>
+            <a href="users.php" class="list-group-item list-group-item-action bg-light">Users</a>
+
+        </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Hi <?= $_SESSION['userName'] ?>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="userForm.php?userID=<?= $_SESSION['nUserID'] ?>">Profile</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="doLogout.php">Sign out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
