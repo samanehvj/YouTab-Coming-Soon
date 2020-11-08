22744b8e783371613b24e8534615c3918b8186a1
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 13:28:29 -0800

    Add header for dashboard area

---
+++ app/views/admin/layouts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>YouTab Admin Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="assets/css/signin.css"> -->
+    <!-- <link rel="stylesheet" href="assets/css/dashboard.css"> -->
+    <link rel="stylesheet" href="/css/simple-sidebar.css">
+</head>
+
+<body>
