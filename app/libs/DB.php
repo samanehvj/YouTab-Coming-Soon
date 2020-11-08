542b1333c7720601b3eadfc57996549f5a7423b5
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 10:50:28 -0800

    Change DB to use pdo connection

--- app/libs/DB.php
+++ app/libs/DB.php
@@ -1,52 +1,82 @@
 <?php
 
+/**
+ * https://github.com/Darynazar/MVCFramework/blob/master/mvcframework/app/libraries/Database.php
+ */
 class DB
 {
+    private $dbHost = 'localhost:8889';
+    private $dbUser = 'root';
+    private $dbPass = 'root';
+    private $dbName = 'youtab_coming';
 
-    public $debug = false;
+    private $statement;
+    private $dbHandler;
+    private $error;
 
-    public function connect()
+    public function __construct()
     {
-        return mysqli_connect('localhost', 'root', 'root', 'youtab_coming');
-    }
-
-    static public function query($sql)
-    {
-
-        $oDB = new DB();
-
-        if ($oDB->debug) {
-            $oDB->debug($sql);
-        }
-
-        $results = mysqli_query($oDB->connect(), $sql);
-
-        if ($results) {
-            $data = null;
-            while ($row = mysqli_fetch_assoc($results)) {
-                $data[] = $row;
-            }
-
-            return $data;
+        $conn = 'mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        try {
+            $this->dbHandler = new PDO($conn, $this->dbUser, $this->dbPass, $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+            echo $this->error;
         }
     }
 
-    public static function insert($sql)
+    //Allows us to write queries
+    public function query($sql)
     {
-
-        $oDB = new DB();
-
-        if ($oDB->debug) {
-            $oDB->debug($sql);
-        }
-
-        // if insert successfully, it will return true otherwise return false.
-        $con = $oDB->connect();
-        return mysqli_query($con, $sql);
+        $this->statement = $this->dbHandler->prepare($sql);
     }
 
-    public function debug($sql)
+    //Bind values
+    public function bind($parameter, $value, $type = null)
     {
-        echo "<script>console.log('$sql')</script>";
+        switch (is_null($type)) {
+            case is_int($value):
+                $type = PDO::PARAM_INT;
+                break;
+            case is_bool($value):
+                $type = PDO::PARAM_BOOL;
+                break;
+            case is_null($value):
+                $type = PDO::PARAM_NULL;
+                break;
+            default:
+                $type = PDO::PARAM_STR;
+        }
+        $this->statement->bindValue($parameter, $value, $type);
+    }
+
+    //Execute the prepared statement
+    public function execute()
+    {
+        return $this->statement->execute();
+    }
+
+    //Return an array
+    public function resultSet()
+    {
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //Return a specific row as an object
+    public function single()
+    {
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    //Get's the row count
+    public function rowCount()
+    {
+        return $this->statement->rowCount();
     }
 }
