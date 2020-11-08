83d65aeb894b56b8e9c0c1602bc7ed7875155e4d
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 11:12:09 -0800

    Add doLogin to HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -36,4 +36,24 @@ class HomeController extends Controller
         $this->setView('pageNotFound');
         $this->view->render();
     }
+
+    public function doLogIn()
+    {
+
+        $userModel = $this->model('User');
+
+        $user = $userModel->LogIn($_POST["username"], $_POST["password"]);
+
+        $_SESSION["userId"] = $user['id'];
+        $_SESSION["userIsAdmin"] = $user['is_admin'];
+
+        if ($_SESSION["userId"] && $_SESSION['userIsAdmin']) {
+            echo "hello admin";
+            // $this->go("user", "main"); // if details entered exist in the db allow user to login
+        } else {
+            //$this->loadView("views/login.php");
+            // $this->go("public", "errorLogin"); // if details entered do not exist in the db redirect user back to login form with error
+            echo "not admin";
+        }
+    }
 }
