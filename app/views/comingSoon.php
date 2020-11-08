d6228e9a8a1120ce75d5f0b60b267ba925c14102
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 10:54:36 -0800

    Show subscriber informations on submit form

--- app/views/comingSoon.php
+++ app/views/comingSoon.php
@@ -23,7 +23,8 @@
                     <img src="/imgs/logo-black.png" alt="logo" width="50%">
                 </div>
                 <h1>
-                    Nice to meet you
+                    Nice to <br>
+                    meet you
                 </h1>
                 <p>Our website is under construction, but we are ready to go! We are preparing something High-end
                     brand,amazing and exciting for you. Special surprise for our Contact only.</p>
@@ -41,41 +42,63 @@
                 <div class="form-back-btn">
                     <a href="#greeting"><i class="fas fa-arrow-left"></i></a>
                 </div>
-
-                <div class="form-head">
-                    <h2>STAY IN Touch</h2>
-                    <p>
-                        Want to know more about us and our brand, feel free to sign up to attend our launch party.
-
-                    </p>
-                </div>
-
-                <form action="default.html" method="POST" enctype="multipart/form-data">
-                    <div class="form-input">
-                        <div class="input-row">
-                            <input type="text" required name="fname" placeholder="First Name" autocomplete="off">
-                            <input type="text" required name="lname" placeholder="Last Name" autocomplete="off">
-                        </div>
-
-                        <div class="input-row">
-                            <input type="text" required name="phone" placeholder="Phone" autocomplete="off">
-                            <input type="email" required name="email" placeholder="Email" autocomplete="off">
-                        </div>
-
-                        <div class="input-row">
-                            <input type="text" required name="birthdate" placeholder="Your Birthdate" onfocus="(this.type='date')" autocomplete="off">
-                            <input type="text" required name="country" placeholder="Country" autocomplete="off">
-                        </div>
-
-                        <div class="file-row">
-                            <input type="file" required name="image" id="">
-                        </div>
-
-                        <div class="submit-row">
-                            <button type="submit">Submit</button>
-                        </div>
+                <?php if (isset($_SESSION['success']) && !empty($_SESSION['subscriber'])) : ?>
+                    <div>
+                        <h1>Thank You for Register</h1>
                     </div>
-                </form>
+                    <div class="subs-card">
+                        <img src="<?= $_SESSION['subscriber']['image'] ?>" width="200" alt="">
+                        <h2><?= ucfirst($_SESSION['subscriber']['fname']) . " " .  ucfirst($_SESSION['subscriber']['lname']) ?></h2>
+                        <h3>Birthdate: <?= $_SESSION['subscriber']['birthdate'] ?></h3>
+                        <p>
+                            Phone: <?= $_SESSION['subscriber']['phone'] ?>
+                        </p>
+
+                        <p>
+                            Email: <?= $_SESSION['subscriber']['email'] ?>
+                        </p>
+
+                        <p>
+                            From: <?= $_SESSION['subscriber']['country'] ?>
+                        </p>
+                    </div>
+
+                <?php else : ?>
+                    <div class="form-head">
+                        <h2>STAY IN TOUCH</h2>
+                        <p>
+                            Want to know more about us and our brand, feel free to sign up to attend our launch party.
+
+                        </p>
+                    </div>
+
+                    <form action="/subscriber/add" method="POST" enctype="multipart/form-data">
+                        <div class="form-input">
+                            <div class="input-row">
+                                <input type="text" required name="fname" placeholder="First Name" autocomplete="off">
+                                <input type="text" required name="lname" placeholder="Last Name" autocomplete="off">
+                            </div>
+
+                            <div class="input-row">
+                                <input type="text" required name="phone" placeholder="Phone" autocomplete="off">
+                                <input type="email" required name="email" placeholder="Email" autocomplete="off">
+                            </div>
+
+                            <div class="input-row">
+                                <input type="text" required name="birthdate" placeholder="Your Birthdate" onfocus="(this.type='date')" autocomplete="off">
+                                <input type="text" required name="country" placeholder="Country" autocomplete="off">
+                            </div>
+
+                            <div class="file-row">
+                                <input type="file" required name="image" id="">
+                            </div>
+
+                            <div class="submit-row">
+                                <button type="submit">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                <?php endif; ?>
 
             </div>
         </div>
