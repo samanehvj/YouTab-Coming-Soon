eec3dfac05e9ad5bcdbd370e9e68962d1036531d
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 10:49:43 -0800

    Create model method  to return a model object in Controller.php

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -8,4 +8,11 @@ class Controller
         $this->view = new View($viewFile, $viewData);
         return $this->view;
     }
+
+    public function model($model)
+    {
+        $model = ucfirst($model);
+        require_once MODEL . $model . '.php';
+        return new $model;
+    }
 }
