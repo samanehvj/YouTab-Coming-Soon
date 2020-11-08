11dc2590f702cf9af989ee043a327c69053ac037
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-08 10:51:57 -0800

    Create add method in SUbscriber model to insert subscriber  in database

--- app/models/Subscriber.php
+++ app/models/Subscriber.php
@@ -2,8 +2,29 @@
 
 class Subscriber
 {
+    private $db;
+
     public function __construct()
     {
-        echo "Insert into subscriber";
+        $this->db = new DB;
+    }
+    public function add($subscriberForm)
+    {
+        $sql = "INSERT INTO subscribers 
+        (first_name, last_name, phone, email, birthdate, country, image) 
+        values 
+        (:fname, :lname, :phone, :email, :birthdate, :country, :image)";
+
+        $this->db->query($sql);
+
+        $this->db->bind(':fname', $subscriberForm['fname']);
+        $this->db->bind(':lname', $subscriberForm['lname']);
+        $this->db->bind(':phone', $subscriberForm['phone']);
+        $this->db->bind(':email', $subscriberForm['email']);
+        $this->db->bind(':birthdate', $subscriberForm['birthdate']);
+        $this->db->bind(':country', $subscriberForm['country']);
+        $this->db->bind(':image', $subscriberForm['image']);
+
+        return $this->db->execute();
     }
 }
