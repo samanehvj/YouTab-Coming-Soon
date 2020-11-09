002e26c692006da0fd5540c332330274b6497b8e
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-09 07:59:39 -0800

    Clean admin dashboard to show only number of subscribers

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -5,24 +5,9 @@ include VIEW . 'admin/layouts/sidebar.php';
 
 ?>
 <div class="table-responsive">
-    <!-- <table class="table table-striped table-sm">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>View</th>
-            </tr>
-        </thead>
-        <tbody>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tbody>
-    </table> -->
+
     <p> You Have <?= $this->viewData['subscriberCount'] ?> Subscriber</p>
-    <pre>
-        <?= print_r($this->viewData['subscribers']) ?>
-    </pre>
+
 </div>
 <?php
 
